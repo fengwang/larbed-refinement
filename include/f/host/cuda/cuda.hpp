65b12cd72e42c38a0fdad35fa0a72cdeb801a30b
feng@mac <wang_feng@live.com>
2016-06-21 15:05:47 +0200

    adding missed headers

---
+++ include/f/host/cuda/cuda.hpp
@@ -0,0 +1,12 @@
+#ifndef SAFLNGMELGUGVUCDPRKXLHLCXMOCUUOXMSTEOLBQJGEBXFNYHFMULVCUMMRIQBDUWOCTVVPQK
+#define SAFLNGMELGUGVUCDPRKXLHLCXMOCUUOXMSTEOLBQJGEBXFNYHFMULVCUMMRIQBDUWOCTVVPQK
+
+#include <f/host/cuda/cuda_runtime.hpp>
+#include <f/host/cuda/cuda_assert.hpp>
+#include <f/host/cuda/cublas.hpp>
+#include <f/host/cuda/cublas_assert.hpp>
+#include <f/host/cuda/cublas_type.hpp>
+#include <f/host/cuda/cuda_utility.hpp>
+
+#endif//SAFLNGMELGUGVUCDPRKXLHLCXMOCUUOXMSTEOLBQJGEBXFNYHFMULVCUMMRIQBDUWOCTVVPQK
+
