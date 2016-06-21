494188de43cd58e1e7b6c17621c8663e8766933c
feng@mac <wang_feng@live.com>
2016-06-21 15:04:28 +0200

    tmp commit

--- src/direct_cuda_pattern.cc
+++ src/direct_cuda_pattern.cc
@@ -2,8 +2,6 @@
 #include <f/pattern/pattern.hpp>
 #include <f/cuda_pattern/cuda_pattern.hpp>
 #include <f/date/date_to_string.hpp>
-#include <f/variate_generator/variate_generator.hpp>
-#include <f/pattern_refinement/reflection_residual.hpp>
 
 #include <vector>
 #include <iostream>
@@ -12,44 +10,36 @@
 
 int main()
 {
-    auto pt = f::make_pattern( "testdata/new_txt", std::complex<double>{0.0, 12.0} );
-    f::cuda_pattern cpt{ pt, 3 };
+    int const thickness_ = 14.5;
+    double const init_thickness = thickness_;
+    std::complex<double> const thickness{ 0.0, init_thickness  };
+    auto pt = f::make_pattern("testdata/new_txt", thickness);
 
+    f::cuda_pattern cpt{ pt, 1 };
+    
     std::vector<double> ug_initial;
     ug_initial.resize( pt.ug_size*2 + 1 );
-    std::fill( ug_initial.begin(), ug_initial.end(), 0.00 );
+    std::fill( ug_initial.begin(), ug_initial.end(), 0.01 );
+    ug_initial[0] = 1.0;
+    ug_initial[1] = 0.0;
+    ug_initial[pt.ug_size*2] = init_thickness;
 
-    unsigned long const diffraction_dim = 121;
-
-    f::variate_generator<double> vg( 0.0, 0.01 );
-    std::generate( ug_initial.begin(), ug_initial.end(), vg );
-
-    //Enforcing Friedel's law
-    f::reflection_residual rr{ std::string{"./matrix/beam/SrTiO3.txt"}, std::string{"./matrix/intensity/SrTiO3.txt"}, pt.ug_size };
-    auto const& reflection_merit = rr.make_normal_residual();
-
-    auto const& merit_function_ = cpt.make_abs_function(); 
-
-    unsigned long const unknowns = pt.ug_size*2+1;
-    unsigned long const zos = pt.ug_size - 1;
-    auto const& merit_function = [&reflection_merit, &merit_function_, unknowns, zos]( double* x )
-    {
-        double const res = std::inner_product( x, x+unknowns, x, 0.0 );
-
-        return reflection_merit( x ) + merit_function_( x ) + res * 100.9;
-    };
+    auto const& merit_function = cpt.make_merit_function(); 
 
     f::simple_steepest_descent<double> sd( merit_function, pt.ug_size * 2 + 1 );
     sd.config_initial_guess( ug_initial.begin() );
-    sd.config_total_steps( 2000 );
+    sd.config_total_steps( 1000 );
     sd.config_eps( 1.0e-10 );
 
-    std::string file_name = f::date_to_string() + std::string{"_direct_cuda_pattern.dat"};
-    std::string abs_file_name = f::date_to_string() + std::string{"_direct_cuda_pattern_R.dat"};
+    std::string tk = std::to_string(thickness_);
+    std::string file_name = f::date_to_string() + std::string{"-"} + tk + std::string{"_direct_cuda_pattern.dat"};
+    std::string abs_file_name = f::date_to_string() + std::string{"-"} + tk +  std::string{"_direct_cuda_pattern_abs.dat"};
+
+    const unsigned long unknowns = pt.ug_size * 2 + 1;
 
     auto const& abs_function = cpt.make_abs_function();
 
-    auto on_iteration_over = [ &file_name, &abs_file_name, unknowns, &merit_function_ ]( double residual, double* current_solution )
+    auto on_iteration_over = [ &file_name, &abs_file_name, unknowns, &abs_function ]( double residual, double* current_solution )
     {
         std::ofstream ofs1( file_name.c_str(), std::fstream::app );
         ofs1 << residual << "\t";
@@ -58,7 +48,7 @@ int main()
         ofs1.close();
 
         std::ofstream ofs2( abs_file_name.c_str(), std::fstream::app );
-        double const abs_residual =  merit_function_( current_solution );
+        double const abs_residual =  abs_function( current_solution );
         ofs2 << abs_residual << "\t";
         std::copy( current_solution, current_solution+unknowns, std::ostream_iterator<double>( ofs2, "\t" ) );
         ofs2 << "\n";
